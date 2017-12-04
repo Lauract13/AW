959ef1b10741e4aee9a01d27b4075c455ee069ca
José Antonio Bernal Pérez <trjobp@gmv.es>
2017-12-04 17:53:45 +0100

    Added bodyparser

--- Practica 1/server.js
+++ Practica 1/server.js
@@ -3,15 +3,25 @@
 const http = require("http");
 const express = require("express");
 const path = require("path");
+var bodyParser = require("body-parser");
+const morgan = require("morgan");
 const app = express();
 const staticFiles = path.join(__dirname, "public");
 app.use(express.static(staticFiles));
+app.use(morgan("dev"));
 app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
 app.get("/", (request, response) => {
     response.redirect("users/login.html");
-})
+});
+
+app.post("/users/loginpost.html", bodyParser.urlencoded({extended:false}), function(request, response){
+    console.log(request.body);
+    response.end();
+});
 
 app.listen(3000, (err) => {
-    console.log(err);
+    if(err)
+        console.log(err);
 });
