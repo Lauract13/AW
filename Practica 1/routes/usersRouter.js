17e5f6f78e1f24b9a5c86b949baa4950a2f860d8
JOSE ANTONIO BERNAL PEREZ <josebern@ucm.es>
2017-12-10 18:49:37 +0100

    Added search functionality

--- Practica 1/routes/usersRouter.js
+++ Practica 1/routes/usersRouter.js
@@ -117,7 +117,54 @@ usersRouter.get("/amigos.html", (request, response) => {
             puntos: 0
         });
     }
-})
+});
+
+usersRouter.get("/search", (request, response) => {
+    let loggedIn = (String(request.session.user) !== 'undefined');
+    if (!loggedIn) {
+        response.redirect("/users/login.html");
+    } else {
+        pool.getConnection((err, conn) => {
+            if (err) { console.log("Connection error"); } else {
+                let sql = "SELECT email, name, image FROM users WHERE name LIKE ?";
+                let name = '%' + request.query.name + '%';
+                conn.query(sql, [name], (err, res, fields) => {
+                    if (err) {
+                        console.log(err);
+                    } else {
+                        let usersArray = [];
+                        console.log(res);
+                        if (res.length > 0) {
+                            res.forEach((user) => {
+                                let userImage;
+                                let str = path.join(__dirname, "..", "public", "icons", String(user.image));
+                                console.log(str);
+                                if (fs.existsSync(str)) {
+                                    userImage = path.join("..", "icons", String(user.image));
+                                } else {
+                                    userImage = path.join("..", "img", "NoProfile.png");
+                                }
+                                let aux = {
+                                    email: user.email,
+                                    user: user.name,
+                                    image: userImage
+                                }
+                                usersArray.push(aux);
+                            });
+                        }
+                        console.log(usersArray);
+                        response.render("search.ejs", {
+                            user: loggedIn,
+                            image: request.session.image,
+                            puntos: 0,
+                            users: usersArray
+                        });
+                    }
+                });
+            }
+        });
+    }
+});
 
 usersRouter.post("/loginpost", function(request, response) {
     pool.getConnection((err, conn) => {
