d3bc59447fc1388fcc79c07b59720d870b02298a
JOSE ANTONIO BERNAL PEREZ <josebern@ucm.es>
2017-12-16 21:55:30 +0100

    BBDD sql

--- Practica 1/facebluff.sql
+++ Practica 1/facebluff.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 13-12-2017 a las 19:10:51
+-- Tiempo de generación: 16-12-2017 a las 21:54:38
 -- Versión del servidor: 10.1.28-MariaDB
 -- Versión de PHP: 7.1.11
 
@@ -31,16 +31,8 @@ SET time_zone = "+00:00";
 CREATE TABLE `friends` (
   `email1` varchar(20) NOT NULL,
   `email2` varchar(20) NOT NULL,
-  `aceptado` boolean
+  `accepted` tinyint(1) NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Volcado de datos para la tabla `friends`
---
-
-INSERT INTO `friends` (`email1`, `email2`, `aceptado`) VALUES
-('adios@gmail.com', 'holi@gmail.com', 0),
-('adios@gmail.com', 'jeje@gmail.com', 1);
 
 -- --------------------------------------------------------
 
@@ -49,18 +41,10 @@ INSERT INTO `friends` (`email1`, `email2`, `aceptado`) VALUES
 --
 
 CREATE TABLE `sessions` (
-  `session_id` varchar(50) NOT NULL,
-  `expires` int(30) NOT NULL,
-  `data` varchar(500) NOT NULL
+  `session_id` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
+  `expires` int(11) UNSIGNED NOT NULL,
+  `data` text CHARACTER SET utf8mb4 COLLATE utf8mb4_bin
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Volcado de datos para la tabla `sessions`
---
-
-INSERT INTO `sessions` (`session_id`, `expires`, `data`) VALUES
-('X-os6PSxc_fQdUAAJx4m1Fh0TR_IuP0u', 1513274705, '{\"cookie\":{\"originalMaxAge\":null,\"expires\":null,\"httpOnly\":true,\"path\":\"/\"},\"user\":\"holi@gmail.com\",\"name\":\"Jose Antonio\",\"gender\":\"Masculino\",\"image\":\"Diablo-01.png\",\"birthDate\":\"2017-12-05T23:00:00.000Z\"}');
-
 -- --------------------------------------------------------
 
 --
@@ -75,16 +59,6 @@ CREATE TABLE `users` (
   `image` varchar(30) DEFAULT NULL,
   `birthDate` date DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Volcado de datos para la tabla `users`
---
-
-INSERT INTO `users` (`email`, `password`, `name`, `gender`, `image`, `birthDate`) VALUES
-('adios@gmail.com', 'aaaaa', 'Jose', 'Masculino', 'Bat-01.png', NULL),
-('holi@gmail.com', 'aaabbbccc', 'Jose Antonio', 'Masculino', 'Diablo-01.png', '2017-12-06'),
-('jeje@gmail.com', 'aaaa', 'pepe', 'Masculino', 'Diablo-01.png', '2017-12-06');
-
 --
 -- Índices para tablas volcadas
 --
@@ -93,8 +67,14 @@ INSERT INTO `users` (`email`, `password`, `name`, `gender`, `image`, `birthDate`
 -- Indices de la tabla `friends`
 --
 ALTER TABLE `friends`
-  ADD KEY `FK_AMIGOS_1` (`email1`),
-  ADD KEY `FK_AMIGOS_2` (`email2`);
+  ADD PRIMARY KEY (`email1`,`email2`),
+  ADD KEY `FK_FRIENDS_2` (`email2`);
+
+--
+-- Indices de la tabla `sessions`
+--
+ALTER TABLE `sessions`
+  ADD PRIMARY KEY (`session_id`);
 
 --
 -- Indices de la tabla `users`
@@ -111,8 +91,8 @@ ALTER TABLE `users`
 -- Filtros para la tabla `friends`
 --
 ALTER TABLE `friends`
-  ADD CONSTRAINT `FK_AMIGOS_1` FOREIGN KEY (`email1`) REFERENCES `users` (`email`),
-  ADD CONSTRAINT `FK_AMIGOS_2` FOREIGN KEY (`email2`) REFERENCES `users` (`email`);
+  ADD CONSTRAINT `FK_FRIENDS_1` FOREIGN KEY (`email1`) REFERENCES `users` (`email`),
+  ADD CONSTRAINT `FK_FRIENDS_2` FOREIGN KEY (`email2`) REFERENCES `users` (`email`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
