3c346b263db1dd99a3643bf773424b9bd9273b1f
JOSE ANTONIO BERNAL PEREZ <josebern@ucm.es>
2017-12-17 19:28:38 +0100

    BBDD

--- Practica 1/facebluff.sql
+++ Practica 1/facebluff.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 17-12-2017 a las 14:17:01
+-- Tiempo de generación: 17-12-2017 a las 19:28:28
 -- Versión del servidor: 10.1.28-MariaDB
 -- Versión de PHP: 7.1.11
 
@@ -72,6 +72,26 @@ INSERT INTO `friends` (`email1`, `email2`, `accepted`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `guessed`
+--
+
+CREATE TABLE `guessed` (
+  `email1` varchar(20) NOT NULL,
+  `email2` varchar(20) NOT NULL,
+  `question_id` int(20) NOT NULL,
+  `guessed` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `guessed`
+--
+
+INSERT INTO `guessed` (`email1`, `email2`, `question_id`, `guessed`) VALUES
+('adios@gmail.com', 'abc@gmail.com', 1, 1);
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `questions`
 --
 
@@ -88,7 +108,10 @@ CREATE TABLE `questions` (
 INSERT INTO `questions` (`id`, `pregunta`, `respuestas`) VALUES
 (1, 'gfdsregaeg', 'fhagvkwjç,fbhajkvjf,gfdsahbg'),
 (2, 'hdfthsth', 'garigbla,gauybsdg,fsauygsf,gfudislhg'),
-(3, 'Hola', 'Que tal,loco,como vas,jaja,Mira tio vale ya joder');
+(3, 'Hola', 'Que tal,loco,como vas,jaja,Mira tio vale ya joder'),
+(4, 'rdthdrt', 'hsthsh,srthrt'),
+(5, 'awerawef', 'jnhdyh,ftghrdt'),
+(6, 'aseawef', 'mjhgfnhfg,ghkgyuk,dftyhrt');
 
 -- --------------------------------------------------------
 
@@ -107,8 +130,8 @@ CREATE TABLE `sessions` (
 --
 
 INSERT INTO `sessions` (`session_id`, `expires`, `data`) VALUES
-('WP2hSMMX9-cC65Mraat96wEQpzP7ssBv', 1513550498, '{\"cookie\":{\"originalMaxAge\":null,\"expires\":null,\"httpOnly\":true,\"path\":\"/\"},\"user\":\"adios@gmail.com\",\"name\":\"Jose\",\"password\":\"aaaaa\",\"gender\":\"Masculino\",\"image\":\"Bat-01.png\",\"birthDate\":null}'),
-('qT4kA4fhi1YBQOoAUgqfSlsChZbgpM2B', 1513602993, '{\"cookie\":{\"originalMaxAge\":null,\"expires\":null,\"httpOnly\":true,\"path\":\"/\"},\"user\":\"abc@gmail.com\",\"name\":\"BIG SHAQ\",\"password\":\"aaaaa\",\"gender\":\"Masculino\",\"image\":\"Frankenstein-01.png\",\"birthDate\":\"2014-02-03T23:00:00.000Z\"}');
+('-Rgjpxbi-S-Y939NKt0DC6-29TqGl4pS', 1513619795, '{\"cookie\":{\"originalMaxAge\":null,\"expires\":null,\"httpOnly\":true,\"path\":\"/\"},\"user\":\"adios@gmail.com\",\"name\":\"Jose\",\"password\":\"aaaaa\",\"gender\":\"Masculino\",\"image\":\"Bat-01.png\",\"birthDate\":null,\"points\":80}'),
+('WP2hSMMX9-cC65Mraat96wEQpzP7ssBv', 1513550498, '{\"cookie\":{\"originalMaxAge\":null,\"expires\":null,\"httpOnly\":true,\"path\":\"/\"},\"user\":\"adios@gmail.com\",\"name\":\"Jose\",\"password\":\"aaaaa\",\"gender\":\"Masculino\",\"image\":\"Bat-01.png\",\"birthDate\":null}');
 
 -- --------------------------------------------------------
 
@@ -122,20 +145,21 @@ CREATE TABLE `users` (
   `name` varchar(20) NOT NULL,
   `gender` varchar(20) NOT NULL,
   `image` varchar(30) DEFAULT NULL,
-  `birthDate` date DEFAULT NULL
+  `birthDate` date DEFAULT NULL,
+  `points` int(20) NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Volcado de datos para la tabla `users`
 --
 
-INSERT INTO `users` (`email`, `password`, `name`, `gender`, `image`, `birthDate`) VALUES
-('abc@gmail.com', 'aaaaa', 'BIG SHAQ', 'Masculino', 'Frankenstein-01.png', '2014-02-04'),
-('adios@gmail.com', 'aaaaa', 'Jose', 'Masculino', 'Bat-01.png', NULL),
-('dfhdshr', 'bbb', 'ASHNEEF', 'Masculino', 'Harley-01.png', NULL),
-('holi@gmail.com', 'aaabbbccc', 'Jose Antonio', 'Masculino', 'Diablo-01.png', '2017-12-06'),
-('kkk@gmail.com', 'kkk', 'KKKKK', 'Masculino', 'DLy97FQW0AAQIci.jpg', NULL),
-('lmao@gmail.com', 'lmao', 'PAUL', 'Femenino', 'npp', NULL);
+INSERT INTO `users` (`email`, `password`, `name`, `gender`, `image`, `birthDate`, `points`) VALUES
+('abc@gmail.com', 'aaaaa', 'BIG SHAQ', 'Masculino', 'Frankenstein-01.png', '2014-02-04', 0),
+('adios@gmail.com', 'aaaaa', 'Jose', 'Masculino', 'Bat-01.png', NULL, 80),
+('dfhdshr', 'bbb', 'ASHNEEF', 'Masculino', 'Harley-01.png', NULL, 0),
+('holi@gmail.com', 'aaabbbccc', 'Jose Antonio', 'Masculino', 'Diablo-01.png', '2017-12-06', 0),
+('kkk@gmail.com', 'kkk', 'KKKKK', 'Masculino', 'DLy97FQW0AAQIci.jpg', NULL, 0),
+('lmao@gmail.com', 'lmao', 'PAUL', 'Femenino', 'npp', NULL, 0);
 
 --
 -- Índices para tablas volcadas
@@ -154,6 +178,14 @@ ALTER TABLE `answers`
 ALTER TABLE `friends`
   ADD PRIMARY KEY (`email1`,`email2`),
   ADD KEY `FK_FRIENDS_2` (`email2`);
+
+--
+-- Indices de la tabla `guessed`
+--
+ALTER TABLE `guessed`
+  ADD PRIMARY KEY (`email1`,`email2`,`question_id`),
+  ADD KEY `FK_GUESSED_2` (`email2`),
+  ADD KEY `FK_GUESSED_3` (`question_id`);
 
 --
 -- Indices de la tabla `questions`
@@ -182,7 +214,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT de la tabla `questions`
 --
 ALTER TABLE `questions`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- Restricciones para tablas volcadas
@@ -201,6 +233,14 @@ ALTER TABLE `answers`
 ALTER TABLE `friends`
   ADD CONSTRAINT `FK_FRIENDS_1` FOREIGN KEY (`email1`) REFERENCES `users` (`email`),
   ADD CONSTRAINT `FK_FRIENDS_2` FOREIGN KEY (`email2`) REFERENCES `users` (`email`);
+
+--
+-- Filtros para la tabla `guessed`
+--
+ALTER TABLE `guessed`
+  ADD CONSTRAINT `FK_GUESSED_1` FOREIGN KEY (`email1`) REFERENCES `users` (`email`),
+  ADD CONSTRAINT `FK_GUESSED_2` FOREIGN KEY (`email2`) REFERENCES `users` (`email`),
+  ADD CONSTRAINT `FK_GUESSED_3` FOREIGN KEY (`question_id`) REFERENCES `questions` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
