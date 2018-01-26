934895475452eb42482cfffd82952e0ff3673ba1
Batchata <lacasa03@ucm.es>
2018-01-26 12:06:02 +0100

    jose la estoy liando mazo teteeeee

--- Practica 2/routes/index.js
+++ Practica 2/routes/index.js
@@ -1,9 +1,39 @@
 var express = require('express');
 var router = express.Router();
+var bodyParser = require("body-parser");
+
+app.use(bodyParser.json());
+
+const pool = mysql.createPool({
+  host: config.dbHost,
+  user: config.dbUser,
+  password: config.dbPassword,
+  database: config.dbName
+});
+
+let dao = new daoUsers(pool);
+
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
+});
+
+app.post("index", function(request, response){
+  var nombre = request.body.nombre;
+  var passwd = request.body.pass;
+
+  if(nombre === undefined || passwd === undefined){
+    response.status(400);
+  }else{
+
+    response.status(201);
+  }
+  //users.push(nombre,passwd);
+  
+  response.end();
+
 });
 
 module.exports = router;
